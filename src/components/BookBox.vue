52bb64eb0851a053967a0cbbe36771e7d87a9cd7
Sush1919 <yamana.sushmanth@gmail.com>
2023-05-06 12:16:50 -0400

    hi

---
+++ src/components/BookBox.vue
@@ -0,0 +1,28 @@
+<template>
+   
+    
+    <div v-for="mobile of mobiles.PhoneDetails" :key="mobile.id" >
+
+     
+       <SingleBook :mobile=mobile />
+
+    </div>
+
+</template>
+<script>
+import SingleBook from './SingleBook.vue'
+
+export default {
+    name: 'BookBox',
+    props:{
+        mobiles: []
+    },
+    components:{
+        SingleBook
+    }
+    
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/SingleBook.vue
@@ -0,0 +1,47 @@
+<template>
+    
+
+    <div>
+
+                        <h2>{{mobile.MobileModel}}</h2>
+                        <h2>{{mobile.ImeiNumber}}</h2>
+                        <h4> {{mobile.ReleaseDate}}</h4>
+                        <h4> {{mobile.Price}}</h4>
+                        <a :href="mobile.Url">Click here</a>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'SingleBook',
+    props:{
+        mobile:Array,
+
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+h4{
+    margin: 2px;
+}
+
+div{
+    background-color: #ffb89e;
+    color: black;
+    padding: 1em;
+    text-align: center;
+    width: 80%;
+    margin: auto;
+    
+
+}
+
+
+
+
+</style>
